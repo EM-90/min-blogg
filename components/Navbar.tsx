1bec4117dfcb95e70af3d8488bf2139265d90267
EM-90 <94170154+EM-90@users.noreply.github.com>
2025-03-16 20:59:57 +0100

    Added lucide icons, fixed navbar to be responsive with hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,21 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+import { Menu, XIcon } from "lucide-react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="  bg-gray-300 w-full flex justify-center">
-      <section className="flex justify-between p-6 bg-gray-200 w-2/3">
+    <nav className="bg-gray-300 flex justify-center relative">
+      <section className="flex justify-between p-6 bg-gray-200 w-full lg:w-2/3">
         <div>UU-Laben</div>
-        <ul className=" flex gap-12">
+        <button
+          className="text-black lg:hidden z-20"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <XIcon size={28} color="#ffffff" /> : <Menu size={28} />}
+        </button>
+        <ul className="hidden lg:flex gap-12">
           <li className=" decoration-0">
             <a href="#">Hjem</a>
           </li>
@@ -22,6 +32,26 @@ function Navbar() {
             <a href="#">Guider</a>
           </li>
         </ul>
+
+        {isOpen && (
+          <ul className="lg:hidden bg-black flex flex-col pt-20 pl-10 text-3xl w-full  space-y-15 text-white  h-screen absolute top-18 right-0">
+            <li className=" decoration-0">
+              <a href="#">Hjem</a>
+            </li>
+            <li className=" decoration-0">
+              <a href="#">Kontakt</a>
+            </li>
+            <li className=" decoration-0">
+              <a href="#">Om</a>
+            </li>
+            <li className=" decoration-0">
+              <a href="#">Artikler</a>
+            </li>
+            <li className=" decoration-0">
+              <a href="#">Guider</a>
+            </li>
+          </ul>
+        )}
       </section>
     </nav>
   );

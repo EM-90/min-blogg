03780d33f7ab0d9146ec2b69b7ef79a8f8df661f
EM-90 <94170154+EM-90@users.noreply.github.com>
2025-03-22 12:36:41 +0100

    Moved Header component into Header folder

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { Header } from "@/components/header/Header";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { Header } from "@/components/header/Header";
 import Link from "next/link";
 
 export default function Home() {
